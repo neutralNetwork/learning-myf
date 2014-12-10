15f463bfd74cdba380a16ddd0e8ee8cd884f1100
Myf Ma <myf@worldtalks.org>
2014-12-09 23:08:36 -0500

    implementing adder

--- cli.js
+++ cli.js
@@ -1,16 +1,26 @@
 var program = require('commander');
 var perceptron = require('./perceptron');
+var adder = require('./adder');
 
 program
-    .version('0.0.1')
+    .version('0.0.1');
 
 program
-    .command('compute <file>')
+    .command('perceptron <file>')
     .description('computing from json input')
     .action(function(file){
         var json = require('./' + file);
-        console.log(perceptron(json))
-    })
+        console.log(perceptron(json.inputs, json.bias));
+    });
+
+program
+    .command('adder <file>')
+    .description('computing from json input')
+    .action(function(file){
+        var json = require('./' + file);
+        console.log(adder(json));
+    });
+
 program.parse(process.argv);
 
 
--- perceptron.js
+++ perceptron.js
@@ -1,13 +1,11 @@
-var perceptron = function(json) {
+var perceptron = function(inputs, bias) {
     // each inputs has:
     // weight w
     // state (0, 1)
     // bias is a numbeir
-    var inputs = json.inputs;
-    var bias = json.bias;
     return inputs
         .map(function(inp){
-            return inp.w * inp.s
+            return inp.w * inp.s;
         })
         .reduce(function(a, b) {
             return a + b;
